f7277c33387582794089c596386cbc5aeeb68aaa
Francesco <francesco.falchi@gmail.com>
2017-11-08 12:28:02 +0100

    Remove file and comments

--- src/script.js
+++ src/script.js
@@ -7,16 +7,6 @@ $(document).ready(function () {
     $('#display_value').html('Your BMI is ' + person.bmiValue);
     $('#display_message').html('and you are '+ person.bmiMessage);
   });
-  /*$('#imperial_calculate').click(function () {
-    $('#calculate').click(function () {
-      var w = parseFloat($('#weight').val());
-      var h = parseFloat($('#height').val());
-      var person = new Person({weight: w, height: h});
-      person.calculate_imperial_bmi();
-      $('#display_value').html('Your BMI is ' + person.bmiValue);
-      $('#display_message').html('and you are '+ person.bmiMessage);
-    });
-  });*/
   $('#checkbox').click(function () {
     $('#calculate').click(function () {
       var w = parseFloat($('#weight').val());
